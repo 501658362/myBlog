2d0929a1d47816c23de2d64efed4f2896a69075b
chenchaopeng <chenchaopeng@ubtour.com>
2016-08-22 17:24:38 +0800

    rename

--- vendor/michelf/php-smartypants/michelf/SmartyPantsTypographer.php
+++ vendor/michelf/php-smartypants/michelf/SmartyPantsTypographer.php
@@ -3,33 +3,34 @@
 # SmartyPants Typographer  -  Smart typography for web sites
 #
 # PHP SmartyPants & Typographer  
-# Copyright (c) 2004-2006 Michel Fortin
-# <http://www.michelf.com/>
+# Copyright (c) 2004-2013 Michel Fortin
+# <http://michelf.ca/>
 #
 # Original SmartyPants
 # Copyright (c) 2003-2004 John Gruber
 # <http://daringfireball.net/>
 #
-namespace michelf;
+namespace Michelf;
+
+
+# Just force Michelf/SmartyPants.php to load. This is needed to load
+# the temporary implementation class. See below for details.
+\Michelf\SmartyPants::SMARTYPANTSLIB_VERSION;
 
 
 #
 # SmartyPants Typographer Parser Class
 #
 # Note: Currently the implementation resides in the temporary class
-# \michelf\_SmartyPantsTypographer_TmpImpl (in the same file as
-# \michelf\SmartyPants). This makes it easier to propagate the changes between
-# the three different packaging styles of PHP Markdown. Once this issue is
+# \Michelf\_SmartyPantsTypographer_TmpImpl (in the same file as
+# \Michelf\SmartyPants). This makes it easier to propagate the changes between
+# the three different packaging styles of PHP SmartyPants. Once this issue is
 # resolved, the _SmartyPantsTypographer_TmpImpl class will disappear and this
 # one will contain the code.
 #
-use \michelf\SmartyPants;
+use \Michelf\SmartyPants;
 
-class SmartyPantsTypographer extends \michelf\_SmartyPantsTypographer_TmpImpl {
-
-	### Version ###
-
-	const  SMARTYPANTSTYPOGRAPHER_VERSION  = \michelf\SMARTYPANTSTYPOGRAPHER_VERSION;
+class SmartyPantsTypographer extends \Michelf\_SmartyPantsTypographer_TmpImpl {
 
 	### Parser Implementation ###
 
@@ -37,5 +38,3 @@ class SmartyPantsTypographer extends \michelf\_SmartyPantsTypographer_TmpImpl {
 	# class. See note above.
 
 }
-
-?>
