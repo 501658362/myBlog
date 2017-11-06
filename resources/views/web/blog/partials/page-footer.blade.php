b2106a72536a570e540c98d4d99d1857c655d1a1
chenchaopeng <chenchaopeng@ubtour.com>
2017-11-06 12:54:59 +0800

    update

--- resources/views/web/blog/partials/page-footer.blade.php
+++ resources/views/web/blog/partials/page-footer.blade.php
@@ -2,8 +2,23 @@
     <hr>
     <div class="container">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-            {{--@include('web.blog.partials.duoshuo')--}}
-            {{--使用网易云跟帖--}}
+
+
+
+
+
+
+
+
+
+
+            @include('web.blog.partials.uyan')
+
+
+
+
+
+
         </div>
     </div>
 @endif
