aa482764eb6ad3ca89238d7099aa5adf8fe8f180
chenchaopeng <chenchaopeng@ubtour.com>
2017-11-14 12:58:20 +0800

    test webhook

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -20,7 +20,7 @@ class TestController extends Controller {
     public function webhook(Request $request) {
         Log::info("git push 啦  该更新代码啦~");
         $data = [];
-        exec('cd /home/wwwroot/chenyanjin.tk/myBlog/ && echo $USER && git pull 2>&1', $data, $data1);
+        exec('/home/wwwroot/chenyanjin.tk/myBlog/updateGit.sh 2>&1', $data, $data1);
         Log::info($data );
         Log::info($data1);
         return view("baidu");
