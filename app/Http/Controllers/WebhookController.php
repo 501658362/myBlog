978a6e7c471e7c295bc75d8391792b405ac2f2af
chenchaopeng <chenchaopeng@ubtour.com>
2017-12-03 01:08:48 +0800

    测试 webhook  参数

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -14,12 +14,13 @@ use Illuminate\Support\Facades\Log;
 class WebhookController extends Controller {
     
     public function gitWebhook(Request $request) {
+        $requestData = $request->all();
         Log::info("git push 啦  该更新代码啦~");
         $data = [];
         exec('cd /home/wwwroot/chenyanjin.tk/myBlog/ && /home/wwwroot/chenyanjin.tk/myBlog/updateGit.sh 2>&1', $data, $data1);
         Log::info($data);
         Log::info($data1);
-        SendEmailService::send_mail(var_export(["执行结果" => $data], true));
+        SendEmailService::send_mail(var_export(["执行结果" => $data, "github 请求" => $requestData], true));
         return response()->json($data);
     }
     
