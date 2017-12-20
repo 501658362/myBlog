feade3459d6c3cbeaf047da0c3075e242f2b4699
chenchaopeng <chenchaopeng@ubtour.com>
2017-12-20 16:44:41 +0800

    优化 webhook 邮件内容格式

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -14,14 +14,14 @@ use Illuminate\Support\Facades\Log;
 class WebhookController extends Controller {
     
     public function gitWebhook(Request $request) {
-        $requestData =  $request->header("X-Hub-Signature");
+        $requestData = $request->header("X-Hub-Signature");
         Log::info("git push 啦  该更新代码啦~");
         $data = [];
         exec('cd /home/wwwroot/chenyanjin.tk/myBlog/ && /home/wwwroot/chenyanjin.tk/myBlog/updateGit.sh 2>&1', $data, $data1);
         Log::info($data);
         Log::info($data1);
-        SendEmailService::send_mail(var_export(["执行结果" => $data, "github 请求" => $requestData], true));
+        $text = "执行结果<br>" . implode("<br>", $data) ."<br>github 请求<>".$requestData;
+        SendEmailService::send_mail($text);
         return response()->json($data);
     }
-    
 }
