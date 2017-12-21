cd30d0c060cd4b4596eafc4f971c3162348f8a0b
chenchaopeng <chenchaopeng@ubtour.com>
2017-12-21 15:22:13 +0800

    webhook 添加 try catch

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -14,24 +14,29 @@ use Illuminate\Support\Facades\Log;
 class WebhookController extends Controller {
     
     public function gitWebhook(Request $request) {
-        $requestData = $request->header("X-Hub-Signature");
-        Log::info("git push 啦  该更新代码啦~");
-        $data = [];
+        try{
+            $requestData = $request->header("X-Hub-Signature");
+            Log::info("git push 啦  该更新代码啦~");
+            $data = [];
     
-        $v = "验证失败";
-        $signature = $requestData;
-        if ($signature) {
-            $hash = "sha1=" . hash_hmac('sha1', $request->getContent(), "test123456");
-            if (strcmp($signature, $hash) == 0) {
-                $v = "验证成功";
-                exec('cd /home/wwwroot/chenyanjin.tk/myBlog/ && /home/wwwroot/chenyanjin.tk/myBlog/updateGit.sh 2>&1', $data, $data1);
-                Log::info($data);
-                Log::info($data1);
+            $v = "验证失败";
+            $signature = $requestData;
+            if ($signature) {
+                $hash = "sha1=" . hash_hmac('sha1', $request->getContent(), "test123456");
+                if (strcmp($signature, $hash) == 0) {
+                    $v = "验证成功";
+                    exec('cd /home/wwwroot/chenyanjin.tk/myBlog/ && /home/wwwroot/chenyanjin.tk/myBlog/updateGit.sh 2>&1', $data, $data1);
+                    Log::info($data);
+                    Log::info($data1);
+                }
             }
+    
+            $text = "执行结果<br>github 请求: $v <br> header X-Hub-Signature:".$requestData."<br>" . implode("<br>", $data) ."<br>";
+            SendEmailService::send_mail($text);
+            return response()->json([$data, $v]);
+        }catch (\Exception $ex){
+            SendEmailService::send_mail($ex->getTraceAsString(), "git webhook 接发，回调发生异常， 发送异常！！！！");
+            return response(true, 500)->json();
         }
-     
-        $text = "执行结果<br>" . implode("<br>", $data) ."<br>github 请求: $v <br> header X-Hub-Signature:".$requestData;
-        SendEmailService::send_mail($text);
-        return response()->json([$data, $v]);
     }
 }
